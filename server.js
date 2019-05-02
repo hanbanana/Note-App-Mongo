251299a8dbad3bd21f8eb72eedb2c6a6c8f579cd
Hansung Lee <hanlee@GOLD-POINT.COM>
2019-05-02 12:00:55 -0700

    setting up to push to Heroku!!!!!

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ var express = require("express");
 var mongojs = require("mongojs");
 var bodyParser = require('body-parser');
 var logger = require('morgan');
-var mongoose = require("mongoose");
 
 
 var app = express()
@@ -15,7 +14,6 @@ app.use(express.static("public"));
 
 app.use(logger("dev"));
 
-
 var databaseUrl = "notetaker_mongo";
 var collections = ["notes"];
 
@@ -25,28 +23,7 @@ db.on("error", function (error) {
     console.log("Database Error: ", error);
 });
 
-// Database configuration with mongoose
-
-var databaseUri = "mongodb://localhost/latimesscrape";
-if (process.env.MONGODB_URI) {
-  mongoose.connect(process.env.MONGODB_URI);
-} else {
-  mongoose.connect(databaseUri);
-}
-var db = mongoose.connection;
-
-mongoose.connect("mongodb://heroku_k4npvtj3:rl8uap6in70hc9bv41u73vgvqt@ds151066.mlab.com:51066/heroku_k4npvtj3");
-var db = mongoose.connection;
-
-// Show any mongoose errors
-db.on("error", function(error) {
-  console.log("Mongoose Error: ", error);
-});
-
-// Once logged in to the db through mongoose, log a success message
-db.once("open", function() {
-  console.log("Mongoose connection successful.");
-});
+mongjs.connect("mongodb://heroku_2pvlqg9x:failhoaef5bn4vo92v4l6bcrba@ds133202.mlab.com:33202/heroku_2pvlqg9x");
 
 
 app.get('/', function (req, res) {
